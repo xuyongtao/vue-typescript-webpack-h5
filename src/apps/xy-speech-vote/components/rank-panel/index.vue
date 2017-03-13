5ab33bc5ea452ed4a29d92f761f413449df7e7c2
yangli <121029539@qq.com>
2017-03-13 13:30:43 +0800

    排行榜样式定好

--- src/apps/xy-speech-vote/components/rank-panel/index.vue
+++ src/apps/xy-speech-vote/components/rank-panel/index.vue
@@ -1,14 +1,90 @@
 <template>
     <div id="xy-speech-vote-rank-panel">
-        排行榜  
-    </div>  
+            <h2>排行榜</h2>
+            <div class="content">
+                <ul>
+                    <li>
+                        <div class="inner">
+                            <a href="javascript:;" class="name">选手名字一</a>
+                            <span class="total">0555599票</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner">
+                            <a href="javascript:;" class="name">选手名字</a>
+                            <span class="total">555599票</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner">
+                            <a href="javascript:;" class="name">选手名字</a>
+                            <span class="total">55599票</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner">
+                            <a href="javascript:;" class="name">选手名字</a>
+                            <span class="total">5599票</span>
+                        </div>
+                    </li>
+                          <li>
+                        <div class="inner one">
+                            <a href="javascript:;" class="name">选手名字一</a>
+                            <span class="total">599票</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner two">
+                            <a href="javascript:;" class="name">选手名字</a>
+                            <span class="total">99票</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner three">
+                            <a href="javascript:;" class="name">选手名字</a>
+                            <span class="total">9票</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner four">
+                            <a href="javascript:;" class="name">选手名字</a>
+                            <span class="total">00555599票</span>
+                        </div>
+                    </li>
+                       <li>
+                        <div class="inner three">
+                            <a href="javascript:;" class="name">选手名字</a>
+                            <span class="total">200555599票</span>
+                        </div>
+                    </li>
+                   <li>
+                        <div class="inner four">
+                            <a href="javascript:;" class="name">选手名字</a>
+                            <span class="total">22900555599票</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner four">
+                            <a href="javascript:;" class="name">选手名字</a>
+                            <span class="total">22900555599票</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner four">
+                            <a href="javascript:;" class="name">选手名字</a>
+                            <span class="total">22900555599票</span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+    </div>
 </template>
 
 <script>
     export default {
         data: function() {
             return {
-                      
+
             }
         }
     }
